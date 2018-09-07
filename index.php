8652a474fd1f6b00d282899d2811717fe3ed5b50
programmeres87 <43022296+programmeres87@users.noreply.github.com>
2018-09-07 16:55:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
-echo "<h3>Recodes!!!</h3>";
+<?php 
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method == 'POST') {
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+	
+// 	$paramGeoCity = ucfirst($json->queryResult->queryText);
+// 	$paramBedroom = $json->queryResult->parameters->bedroom;
+// 	$fulfillmentText = "We have " . rand(1, 1000) . " apartment listings at " . $paramGeoCity . " with " . $paramBedroom;
+	
+// 	$response = new \stdClass();
+// 	$response->fulfillmentText = $fulfillmentText;
+	//$response->displayText = $fulfillmentText;
+	//$response->source = "webhook";
+  
+  $response->messages = "carousel message";
+	echo json_encode($response);
+} else {
+	echo "Method not allowed by els";
+}
 ?>
