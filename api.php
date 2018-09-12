7867fff7fabfff66c0115265e4c54981a8095794
programmeres87 <43022296+programmeres87@users.noreply.github.com>
2018-09-12 10:55:19 +0700

    Create api.php

---
+++ api.php
@@ -0,0 +1,25 @@
+<?php
+header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == 'POST') {
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+	
+// 	$paramGeoCity = ucfirst($json->queryResult->queryText);
+// 	$paramBedroom = $json->queryResult->parameters->bedroom;
+// 	$fulfillmentText = "We have " . rand(1, 1000) . " apartment listings at " . $paramGeoCity . " with " . $paramBedroom;
+		
+// 	$response->fulfillmentText = $fulfillmentText;
+	//$response->displayText = $fulfillmentText;
+	//$response->source = "webhook";
+  
+  $response = new \stdClass();
+  $card = array(array('card' => array('title' => 'john', 'subtitle' => 'john hoge')),
+              array('card' => array('title' => 'jane', 'subtitle' => 'jane hoge')));
+	
+  $response->fulfillmentMessages = $card;
+	echo json_encode($response);
+} else {
+	echo "Method not allowed by els";
+}
+?>
